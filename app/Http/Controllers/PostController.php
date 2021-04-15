c39bcdada3a34ae220b426d6c6a78fffb6fcec19
Marko Djuricic <markodjuricic@Markos-MacBook-Pro.local>
2021-04-15 21:34:47 +0200

    list is_published posts only

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,15 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 
 class PostController extends Controller {
     public function index() {
-        $posts = Post::orderBy('created_at', 'desc')->get();
+        $posts = Post::where('is_published', true)->orderBy('created_at', 'desc')->get();
         return view('posts.index', compact('posts'));
     }
 
     public function show(Post $post) {
+        if (!$post->is_published) {
+            throw new ModelNotFoundException();
+        }
         return view('posts.show', compact('post'));
     }
 }
